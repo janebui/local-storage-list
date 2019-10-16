143e0f01166b517d53230b6f66a328090513c2f0
Jane Bui <janebuidesign@gmail.com>
2019-10-16 10:46:47 -0700

    add remove function

--- script.js
+++ script.js
@@ -1,37 +1,62 @@
 const form = document.querySelector('form');
+const input = document.querySelector('#item');
 const list = document.querySelector('.list');
 const clearButton = document.querySelector('#clear');
-const input = document.querySelector('#item');
-let itemsArray = [];
+const itemsToRemove = document.querySelectorAll('.remove');
+let itemsArray = JSON.parse(localStorage.getItem('items')) || [];
+// retrieve items from localStorage and parse, or empty array if nothing in localStorage yet
 
-// retrieve items from localStorage and parse
-const data = JSON.parse(localStorage.getItem('items')) || [] ;
+// add an item from the input
+function addItem(e) {
+    e.preventDefault();
+    const item = input.value;
+    itemsArray.push(item);
 
-// addItem function appends li 
-const addItem = item => {
-    const li = document.createElement('li');
-    li.textContent = item;
-    list.appendChild(li);
-}
+    populateList(itemsArray, list);
 
-form.addEventListener('submit', function (e) {
-    e.preventDefault() // prevent the form from sending to a server
-
-    itemsArray.push(input.value);
-
-    // store item in localStorage as a string
     localStorage.setItem('items', JSON.stringify(itemsArray));
 
-    addItem(input.value);
     this.reset();
-})
+}
 
-// display local Storage items on front-end list
-data.forEach(item => {
-    addItem(item);
-})
+// populate the list using what's in localStorage
+function populateList(itemsArray = [], list) {
+    list.innerHTML = itemsArray.map((item, i) => {
+        return `
+            <li data-index="${i}">
+                ${item}
+                <i class="far fa-times-circle remove"></i>
+            </li>
+        `;
+    }).join(''); // need to join bc map returns an array, but innerHtml needs a string
+}
 
-clearButton.addEventListener('click', function () {
+// remove an item from the list when clicking X
+function removeItem(e) {
+    if (!e.target.matches('.remove')) return;
+    const li = e.target.parentNode;
+    const index = li.dataset.index;
+    itemsArray.splice(index, 1);
+    localStorage.setItem('items', JSON.stringify(itemsArray)); // update the localStorage
+    populateList(itemsArray, list); // repopulate list with updated items
+}
+
+// remove all items from the list
+function clearAll() {
     localStorage.clear();
     list.innerHTML = '';
-})
+}
+
+// listen to submit event, works with clicking the button or hitting Enter
+form.addEventListener('submit', addItem);
+
+// remove an item from the list when clicking X
+list.addEventListener('click', removeItem);
+
+// clear all items from the list when clicking the Clear All button
+clearButton.addEventListener('click', clearAll);
+
+// populate the list on page load
+populateList(itemsArray, list);
+
+//
